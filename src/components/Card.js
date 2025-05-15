cfe32eb771634ba01d47fa778b6874cf672d9c08
Zavyalov Daniil <dezavv1@gmail.com>
2025-05-15 22:27:48 +0600

    Фикс

---
+++ src/components/Card.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Card.css";
+import Header from "./Header";
+
+export default function Card() {
+  return (
+    <div className="cardStyle animate__animated animate__fadeIn animate__fast">
+      <Header/>
+    </div>
+  );
+}
